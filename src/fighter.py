59984b294ac1cd2c7948b2c21f6a4749240163a8
max-zorn <zhukovma777@gmail.com>
2022-08-26 13:04:22 +0300

    add idle animation

--- src/fighter.py
+++ src/fighter.py
@@ -2,10 +2,16 @@ import pygame
 
 
 class Fighter:
-    def __init__(self, x, y, data, sprite_sheet, animation_steps):
+    def __init__(self, x, y, flip, data, sprite_sheet, animation_steps):
         self.size = data[0]
-        self.flip = False
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.flip = flip
         self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0  # 0:idle, 1:run, 2:jump, 3:attack1, 4:attack2, 5:hit, 6:death
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time = pygame.time.get_ticks()
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
@@ -22,9 +28,13 @@ class Fighter:
                 temp_img = sprite_sheet.subsurface(
                     x * self.size, y * self.size, self.size, self.size
                 )
-                temp_img_list.append(temp_img)
+                temp_img_list.append(
+                    pygame.transform.scale(
+                        temp_img,
+                        (self.size * self.image_scale, self.size * self.image_scale),
+                    )
+                )
             animation_list.append(temp_img_list)
-        print(animation_list)
         return animation_list
 
     def move(self, screen_width, screen_hight, surface, target):
@@ -80,6 +90,19 @@ class Fighter:
         self.rect.x += dx
         self.rect.y += dy
 
+    # handle animation updates
+    def update(self):
+        animation_cooldown = 50
+        self.image = self.animation_list[self.action][self.frame_index]
+        # check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+
+        # check if the animation has finished
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
     def attack(self, surface, target):
         self.attacking = True
         attacking_rect = pygame.Rect(
@@ -94,4 +117,12 @@ class Fighter:
         pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(
+            img,
+            (
+                self.rect.x - (self.offset[0] * self.image_scale),
+                self.rect.y - (self.offset[1] * self.image_scale),
+            ),
+        )
