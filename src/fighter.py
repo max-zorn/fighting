d1b85edd08e575c7f3d8ddd727181b1aa8326073
max-zorn <zhukovma777@gmail.com>
2022-08-26 12:18:10 +0300

    changing the direction of attack depending on the location of the fighter

--- src/fighter.py
+++ src/fighter.py
@@ -3,6 +3,7 @@ import pygame
 
 class Fighter:
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
@@ -53,6 +54,12 @@ class Fighter:
             self.jump = False
             dy = screen_hight - 110 - self.rect.bottom
 
+        # ensure fighters face each other
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
         # update fighter position
         self.rect.x += dx
         self.rect.y += dy
@@ -60,7 +67,10 @@ class Fighter:
     def attack(self, surface, target):
         self.attacking = True
         attacking_rect = pygame.Rect(
-            self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height
+            self.rect.centerx - (2 * self.rect.width * self.flip),
+            self.rect.y,
+            2 * self.rect.width,
+            self.rect.height,
         )
         if attacking_rect.colliderect(target.rect):
             target.health -= 10
