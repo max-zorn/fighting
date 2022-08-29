e64bcbb504e2c5856b81bb2306ed40473cc69476
max-zorn <zhukovma777@gmail.com>
2022-08-29 20:35:18 +0300

    add attack cooldown

--- src/fighter.py
+++ src/fighter.py
@@ -18,6 +18,7 @@ class Fighter:
         self.jump = False
         self.attacking = False
         self.attack_type = 0
+        self.attack_cooldown = 0
         self.health = 100
 
     def load_images(self, sprite_sheet, animation_steps):
@@ -91,6 +92,10 @@ class Fighter:
         else:
             self.flip = True
 
+        # apply attack cooldown
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
+
         # update fighter position
         self.rect.x += dx
         self.rect.y += dy
@@ -124,19 +129,21 @@ class Fighter:
         # check if attack was executed
         if self.action in (3, 4):
             self.attacking = False
+            self.attack_cooldown = 20
 
     def attack(self, surface, target):
-        self.attacking = True
-        attacking_rect = pygame.Rect(
-            self.rect.centerx - (2 * self.rect.width * self.flip),
-            self.rect.y,
-            2 * self.rect.width,
-            self.rect.height,
-        )
-        if attacking_rect.colliderect(target.rect):
-            target.health -= 10
+        if self.attack_cooldown == 0:
+            self.attacking = True
+            attacking_rect = pygame.Rect(
+                self.rect.centerx - (2 * self.rect.width * self.flip),
+                self.rect.y,
+                2 * self.rect.width,
+                self.rect.height,
+            )
+            if attacking_rect.colliderect(target.rect):
+                target.health -= 10
 
-        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+            pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def update_action(self, new_action):
         # check of the new action is different the previous one
