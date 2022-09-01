8d7d9f90713675a1201243b861358091d0b9140b
max-zorn <zhukovma777@gmail.com>
2022-09-01 21:16:47 +0300

    fix sounding.py

--- src/misc/sounding.py
+++ src/misc/sounding.py
@@ -1,20 +1,22 @@
-from pygame import mixer
+from pygame.mixer import init
+from pygame.mixer import Sound
+from pygame.mixer import music
 
 
 def sound_effects(
     game_sound: str,
     p1_weapon_sound: str,
     p2_weapon_sound: str,
-) -> tuple[mixer.Sound, mixer.Sound]:
-    mixer.init()
+) -> tuple[Sound, Sound]:
+    init()
 
-    mixer.music.load(game_sound)
-    mixer.music.set_volume(0.5)
-    mixer.music.play(-1, 0.0, 5000)
+    music.load(game_sound)
+    music.set_volume(0.5)
+    music.play(-1, 0.0, 5000)
 
-    p1_fx = mixer.Sound(p1_weapon_sound)
+    p1_fx = Sound(p1_weapon_sound)
     p1_fx.set_volume(0.5)
 
-    p2_fx = mixer.Sound(p2_weapon_sound)
+    p2_fx = Sound(p2_weapon_sound)
     p2_fx.set_volume(0.5)
     return p1_fx, p2_fx
