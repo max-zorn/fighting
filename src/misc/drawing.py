bff480b6e52991bf203f7164f58a9be4c8e82b3c
max-zorn <zhukovma777@gmail.com>
2022-09-01 12:31:03 +0300

    fix drawing.py

--- src/misc/drawing.py
+++ src/misc/drawing.py
@@ -1,22 +1,37 @@
 from pygame.transform import scale
+from pygame.image import load
 from pygame.draw import rect
+from pygame.font import Font
+from pygame import Surface
+
+from src.config.config import Colors
 
 
-def draw_text(screen, text, font, text_col, x, y):
-    # function for misc text
+def draw_text(
+    screen: Surface,
+    text: str,
+    font: Font,
+    text_col: tuple[int, int, int],
+    x: int,
+    y: int,
+) -> None:
+    # function for drawing text
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
 
-def draw_bg(screen, bg_image, SCREEN_WIDTH, SCREEN_HEIGHT):
-    # function for misc background
-    scale_bg = scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+def draw_bg(screen: Surface, image_path: str, width: int, height: int) -> None:
+    # function for drawing background
+    bg_image = load(image_path).convert_alpha()
+    scale_bg = scale(bg_image, (width, height))
     screen.blit(scale_bg, (0, 0))
 
 
-def draw_health_bar(screen, health, x, y, WHITE, RED, YELLOW):
-    # function for misc fighter health bars
+def draw_health_bar(
+    screen: Surface, health: int, x: int, y: int, colors: Colors
+) -> None:
+    # function for drawing fighter health bars
     ratio = health / 100
-    rect(screen, WHITE, (x - 2, y - 2, 404, 34))
-    rect(screen, RED, (x, y, 400, 30))
-    rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+    rect(screen, colors.white.rgb, (x - 2, y - 2, 404, 34))
+    rect(screen, colors.red.rgb, (x, y, 400, 30))
+    rect(screen, colors.yellow.rgb, (x, y, 400 * ratio, 30))
